f3fd546b518b0ba706c56aa02c27135ea6fb8339
Moch Riyan Putra Suryadi <88395216+riyanslr10@users.noreply.github.com>
2023-10-24 00:49:00 +0700

    update

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2023_A_213040017_mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2023_a_213040017_mvc');
+
+
+?>
---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model {
+    private $nama = "Moch Riyan Putra Suryadi";
+
+    public function getUser() {
+        return $this->nama;
+    }
+}
+
+?>
